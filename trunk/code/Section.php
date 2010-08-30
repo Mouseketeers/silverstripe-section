1244bf6545cd426e27e25c91cd7be773fa0b6489
admin <admin@5766e509-19ac-44c2-bbb0-941299f47fb9>
2010-08-30 14:25:48 +0000

    git-svn-id: svn://10.0.1.208:4000/silverstripe/modules/mouseketeers/section/trunk@210 5766e509-19ac-44c2-bbb0-941299f47fb9

---
+++ trunk/code/Section.php
@@ -0,0 +1,10 @@
+<?php
+class Section extends Page {
+	static $db = array();
+	static $has_one = array();
+	
+	static $icon  = 'section/images/section';
+}
+class Section_Controller extends Page_Controller {
+}
+?>
